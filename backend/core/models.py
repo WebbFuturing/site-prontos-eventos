a89bd87b546089701c2125f2614c363ea6560120
Francisco Jair <fjair123@gmail.com>
2022-04-04 08:44:25 -0300

    bug: Corrigindo os campos do model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,21 +1,22 @@
-from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+
 class Config(models.Model):
     nomeDaConfiguracao = models.CharField(max_length=100)
     navBarCor = models.CharField(max_length=7)
     tituloPag = models.CharField(max_length=100)
     textNavBarCor = models.CharField(max_length=7)
-    sobreOEventoCor = models.CharField(max_length=7)
+    sobreEventoCor = models.CharField(max_length=7)
     textSobreOEventoCor = models.CharField(max_length=100)
-    programacaoCor = models.CharField(max_length=7)
-    textProgramacaoCor = models.CharField(max_length=100)
+    programacaoEventoCor = models.CharField(max_length=7)
+    textProgramacaoEventoCor = models.CharField(max_length=100)
     contatoCor = models.CharField(max_length=7)
     textContatoCor = models.CharField(max_length=100)
 
+
     def __str__(self) -> str:
         return self.nomeDaConfiguracao
+
 
     class Meta():
         verbose_name_plural = "Configurações"
--- backend/core/views.py
+++ backend/core/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from backend.core.models import Config
 
 def index(request):
-    config = Config.objects.get(id=1)
+    #config = Config.objects.get(id=1)
 
     dados = {
-        "titulo" : config.tituloPag,
-        "navCor" : config.navBarCor,
-        "textNavCor" : config.textNavBarCor,
+        "titulo" : "Minha página",
+        "navCor" : "#ffffff",
+        "textNavCor" : "#000000",
     }
 
     return render(request, 'core/index.html', dados)
